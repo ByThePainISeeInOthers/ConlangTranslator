08e51dfd7a7bdc1f83eb7a56f9bc3c08a2edc767
ByThePainISeeInOthers <alexander29072003@gmail.com>
2023-01-15 00:21:01 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(76, 55);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(75, 23);
+            this.btnTranslate.TabIndex = 0;
+            this.btnTranslate.Text = "Перевести";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(33, 29);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(160, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(33, 84);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(160, 20);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 140);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnTranslate);
+            this.Name = "Form1";
+            this.Text = "Словарь";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ Word.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Word
+    {
+        //свойства
+        private string word { get; set; }
+        private string root { get; set; }
+        private string[] preffixes { get; set; }
+        private string[] suffixes { get; set; }
+        private string ending { get; set; }
+        private string eng_word { get; set; }
+        private string eng_root { get; set; }
+        private string[] eng_preffixes { get; set; }
+        private string[] eng_suffixes { get; set; }
+        private string eng_ending { get; set; }
+        //конструкторы
+        public Word() //конструктор по умолчанию
+        {
+            word = "";
+            root = "";
+            preffixes[0] = "";
+            suffixes[0] = "";
+            ending = "";
+            eng_word = "";
+            eng_root = "";
+            eng_preffixes[0] = "";
+            eng_suffixes[0] = "";
+            eng_ending = "";
+        }
+        public Word(string _word, string _root, string[] _preffixes, string[] _suffixes, string _ending, string _eng_word, string _eng_root, string[] _eng_preffixes, string[] _eng_suffixes, string _eng_ending)
+        {
+            word = _word;
+            root = _root;
+            preffixes = _preffixes;
+            preffixes = _suffixes;
+            ending = _ending;
+            eng_word = _eng_word;
+            eng_root = _eng_root;
+            preffixes = _eng_preffixes;
+            preffixes = _eng_suffixes;
+            ending = _eng_ending;
+        }
+        //методы
+        public string Glossing() { return "0"; }
+        public string Deglossing() { return "0"; }
+    }
+}
